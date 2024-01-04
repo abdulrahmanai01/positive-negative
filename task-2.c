e4fa5095771816c84b41a3a94090a344664977d9
Abdul Rahman B <abdulai01@brightskills.com>
2024-01-04 12:13:57 +0530

    add or even

---
+++ task-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value A  :");
+    scanf("%d",&a);
+  if (a%2==0)
+  {
+    printf("value is even number...");
+
+  }
+ else
+ {
+  printf("value is add number...");
+  
+ }
+ return 0;
+}
