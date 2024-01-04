99544aab1b80deb3184f4be48c85beb227a13fcf
Abdul Rahman B <abdulai01@brightskills.com>
2024-01-04 11:12:27 +0530

    logical

---
+++ task-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a;                                                   
+    printf("Enter the value:");
+    scanf("%d",&a);
+
+if (a>0)
+{
+    printf("The value is positive... ");
+}
+else if (a<0)
+{
+    printf("the value is negative...");
+}
+else
+{
+printf("The value is O");
+}
+return 0;
+}
